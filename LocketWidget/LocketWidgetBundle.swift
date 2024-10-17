3f069a9b523e2e6986e221b353f4ed5f0368e237
csiejimmyliu <91661606+csiejimmyliu@users.noreply.github.com>
2024-10-17 13:37:07 +0800

    add widget

---
+++ LocketWidget/LocketWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  LocketWidgetBundle.swift
+//  LocketWidget
+//
+//  Created by 劉旭庭 on 2024/10/17.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct LocketWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        LocketWidget()
+    }
+}
